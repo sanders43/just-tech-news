a85e72eb5e87d1f810e510332f82c9edb82e3265
Waves <64664814+sanders43@users.noreply.github.com>
2020-08-26 15:13:17 -0500

    verify User login

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up mthod to run on instance data to check password this is synchronus but should use asyn for web apps
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 
 // define table columns and configuration
